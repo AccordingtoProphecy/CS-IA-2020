3f1cf2aa860b960f6e2fcfa8e67ac74c24c415e7
Orion Capell <orion.capell@gmail.com>
2020-03-02 19:15:35 -0800

    Formatting

--- finalpagescript.js
+++ finalpagescript.js
@@ -1,11 +1,11 @@
 // Redirect page to game page
 const replayGame = () => {
-    window.location.href = "index.html";
-}
+  window.location.href = "index.html";
+};
 
 // Display final score, located in session storage, when page loads
 window.addEventListener("DOMContentLoaded", event => {
-    let score = sessionStorage.getItem("finalScore");
-    let scoreDIV = document.getElementById("score");
-    scoreDIV.innerHTML = "Final score: " + score;
+  let score = sessionStorage.getItem("finalScore");
+  let scoreDIV = document.getElementById("score");
+  scoreDIV.innerHTML = "Final score: " + score;
 });
